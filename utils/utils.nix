c8383c41667fc7464a05c3d98fbe9d9ebbe3303b
LuoChen <luochen1990@gmail.com>
2025-12-11 20:21:11 +0800

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! wip

--- utils/utils.nix
+++ utils/utils.nix
@@ -2,14 +2,16 @@
 # Adapted from ~/ws/nixos/tools/default.nix pattern
 
 let
-  inherit (import ./list.nix) concatMap for;
   inherit (import ./dict.nix) unionFor;
   inherit (import ./file.nix) findFiles hasPostfix;
 
   # Import modules with function handling
-  importOrCall = path: args:
-    let module = import path;
-    in if builtins.isFunction module then module args else module;
+  importOrCall =
+    path: args:
+    let
+      module = import path;
+    in
+    if builtins.isFunction module then module args else module;
 
 in
 {
@@ -17,36 +19,50 @@ in
     utilsPath:
     let
       # Level 1: builtins-only modules
-      base = unionFor (findFiles (hasPostfix "nix") utilsPath) (path: importOrCall path {});
+      base = unionFor (findFiles (hasPostfix "nix") utilsPath) (path: importOrCall path { });
 
       # Check if more directory exists
       morePath = utilsPath + "/more";
       hasMore = builtins.pathExists morePath;
     in
-    base // {
+    base
+    // {
       more =
         { lib }:
         let
           # Level 2: lib-dependent modules (flatten)
-          level2 = if hasMore then
-            unionFor (findFiles (hasPostfix "nix") morePath) (fname:
-              let result = importOrCall fname { inherit lib; };
-              in if builtins.isAttrs result then result else {
-                "${builtins.replaceStrings [".nix"] [""] (builtins.baseNameOf fname)}" = result;
-              }
-            )
-          else {};
+          level2 =
+            if hasMore then
+              unionFor (findFiles (hasPostfix "nix") morePath) (
+                fname:
+                let
+                  result = importOrCall fname { inherit lib; };
+                in
+                if builtins.isAttrs result then
+                  result
+                else
+                  {
+                    "${builtins.replaceStrings [ ".nix" ] [ "" ] (builtins.baseNameOf fname)}" = result;
+                  }
+              )
+            else
+              { };
 
           moreMorePath = morePath + "/more";
         in
-        base // level2 // {
+        base
+        // level2
+        // {
           more =
             { pkgs }:
             if builtins.pathExists moreMorePath then
               unionFor (findFiles (hasPostfix "nix") moreMorePath) (path: {
-                "${builtins.replaceStrings [".nix"] [""] (builtins.baseNameOf path)}" = importOrCall path { inherit lib pkgs; };
+                "${builtins.replaceStrings [ ".nix" ] [ "" ] (builtins.baseNameOf path)}" = importOrCall path {
+                  inherit lib pkgs;
+                };
               })
-            else {};
+            else
+              { };
         };
     };
 }
